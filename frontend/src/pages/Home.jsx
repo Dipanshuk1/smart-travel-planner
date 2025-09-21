9dfbc2bae48a236404e4c14c3d37c17082815ef6
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-09-21 23:59:25 +0530

    Created Home.jsx file in pages folder

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold">Welcome to Smart Travel Planner 🌍</h1>
+      <p className="mt-4 text-lg">Plan your trips effortlessly with personalized itineraries.</p>
+    </div>
+  );
+}
+export default Home;
